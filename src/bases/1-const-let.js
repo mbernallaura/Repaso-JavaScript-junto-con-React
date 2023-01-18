d7171491c0052d70d5a27d5fcaac3b1a3791b4c1
mbernallaura <mbernallaura@gmail.com>
2023-01-18 17:51:07 -0500

    Se realiza la inicializacion del proyecto de repaso de javascript y se ve, el template string, y tipos de variables

---
+++ src/bases/1-const-let.js
@@ -0,0 +1,21 @@
+//? Variables y Constantes
+
+const nombre = 'Laura';
+const apellido = 'Bernal';
+let valorDado = 5;
+valorDado = 4;
+
+console.log(nombre, apellido, valorDado);
+
+if(true){
+    //let valorDado = 6;
+    console.log(nombre);
+
+    console.log(valorDado);
+}
+
+function prueba(){
+    console.log(valorDado);
+}
+
+prueba();
