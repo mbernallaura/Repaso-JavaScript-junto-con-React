20659df4d198c0470b97abf85b18bd359e9f9421
mbernallaura <mbernallaura@gmail.com>
2023-01-20 18:15:05 -0500

    El uso del fetch(promesa) para utilizar apis

---
+++ src/bases/10-fetch.js
@@ -0,0 +1,12 @@
+const apiKey = 'AiyWk9zuXSArGDI3OCWes1UUE9fHOOxb';
+
+const peticion = fetch(`https://api.giphy.com/v1/gifs/random?api_key=${ apiKey }`);
+peticion.then((resp) => resp.json())
+        .then(({data}) => {
+            const url = data.images.original.url;
+            const img = document.createElement('img');
+            img.src = url;
+            document.body.append(img);
+        })
+
+        .catch(console.warn);
